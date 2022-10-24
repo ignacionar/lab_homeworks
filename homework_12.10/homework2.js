39403e83a5a224e2b9889d88233946b57a8ca5d1
Ignacio Narvaja <85379690+ignacionar@users.noreply.github.com>
2022-10-24 16:13:49 -0700

    Update homework2.js

--- homework_12.10/homework2.js
+++ homework_12.10/homework2.js
@@ -7,23 +7,37 @@ let name = process.argv[3]
 let age = process.argv[4]
 let data = JSON.parse(fs.readFileSync('./db.json'));
 
-const divideAndConquerFn = (array, method) => {
+const divideAndConquerFn = (array) => {
 
-  array.sort();
+  let sortedArray = array.sort((a, b) => {
+    let elA = Object.keys(a);
+    let elB = Object.keys(b);
+    let comparison = 0;
+    if (elA > elB) {
+      comparison = 1;
+    } else {
+      comparison = -1;
+    }
+    return comparison;
+  }) 
 
-  let arrayChunk = array.length / 2;
+  
+  let arrayChunk = sortedArray.length / 2;
 
-  let subArray = array.slice(0, arrayChunk);
-  let subArray2 = array.slice(arrayChunk);
+  let subArray = sortedArray.slice(0, arrayChunk);
+  let subArray2 = sortedArray.slice(arrayChunk);
+
 
   if (method === "SEARCH") {
-    subArray.forEach(element => {
-      for (const key in element) {
-        if (key.includes(name)) {
-          console.log(element)
-        } 
-      }
-    });
+    if (name[0] < subArray2[0]) {
+      return subArray.forEach(element => {
+        for (const key in element) {
+          if (key.includes(name)) {
+            console.log(element);
+          } 
+        }
+      });
+    } 
     subArray2.forEach(element => {
       for (const key in element) {
         if (key.includes(name)) {
@@ -31,59 +45,69 @@ const divideAndConquerFn = (array, method) => {
         } 
       }
     });
+    
   } else if (method === "CREATE") {
     let validation = true;
-    subArray.forEach(element => {
-      for (const key in element) {
-        if (key === name) {
-          validation = false;
-        } 
-      }
-    });
-    subArray2.forEach(element => {
-      for (const key in element) {
-        if (key === name) {
-          validation = false;
-        } 
-      }
-    });
+    if (name[0] < subArray2[0]) {
+      subArray.forEach(element => {
+        for (const key in element) {
+          if (key === name) {
+            validation = false;
+          } 
+        }
+      });
+    }
+    else {
+      subArray2.forEach(element => {
+        for (const key in element) {
+          if (key === name) {
+            validation = false;
+          } 
+        }
+      });
+    }
     if (!validation) {
       return console.log("The name must be unique");
     } 
     let obj = {
       [name]: {age: age}
-    }
+    };
     data.push(obj);
     console.log(data);
     fs.writeFileSync('db.json', JSON.stringify(data));
+    
   } else if (method === "DELETE") {
-    subArray.forEach(element => {
-      for (const key in element) {
-        if (key === (name)) {
-          let index = data.indexOf(element);
-          data.splice(index);
-          fs.writeFileSync('db.json', JSON.stringify(data));
-          console.log(data);
-        } 
-      }
-    })
+    if (name[0] < subArray2[0]) {
+      return subArray.forEach(element => {
+        for (const key in element) {
+          if (key === (name)) {
+            let index = data.indexOf(element);
+            data.splice(index, index);
+            fs.writeFileSync('db.json', JSON.stringify(data));
+            console.log(data);
+          } 
+        }
+      })
+    }
     subArray2.forEach(element => {
       for (const key in element) {
         if (key === (name)) {
           let index = data.indexOf(element);
-          data.splice(index);
+          data.splice(index, index);
           fs.writeFileSync('db.json', JSON.stringify(data));
           console.log(data);
         } 
       }
     })
+    
   } else if (method === "UPDATE") {
-    subArray.forEach(element => {
+    if (name[0] < subArray2[0]) {
+      return subArray.forEach(element => {
         for (const key in element) {
           if (key === (name)) {
             let obj = {
               [name]: {age: age}
-            }
+            };
             let index = data.indexOf(element);
             data.splice(index);
             data[index] = obj;
@@ -92,12 +116,13 @@ const divideAndConquerFn = (array, method) => {
           } 
         }
       })
+    }
     subArray2.forEach(element => {
         for (const key in element) {
           if (key === (name)) {
             let obj = {
               [name]: {age: age}
-            }
+            };
             let index = data.indexOf(element);
             data.splice(index);
             data[index] = obj;
@@ -110,7 +135,7 @@ const divideAndConquerFn = (array, method) => {
 
 }
 
-divideAndConquerFn(data, name, method);
+divideAndConquerFn(data);
 
 
 
